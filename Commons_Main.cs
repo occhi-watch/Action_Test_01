6b33fe792ef3029b30a53cd5c613f70b64315da3
AD\012020048D <reo-kano@sws.com>
2024-06-25 08:43:04 +0900

    【新規】Commons_Main.cs追加

---
+++ Commons_Main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+//===================================================
+// 更新履歴
+//===================================================
+// 2024/06/11 : 012020048D : 新規追加
+//===================================================
+
+public class Commons
+{
+
+
+}
+

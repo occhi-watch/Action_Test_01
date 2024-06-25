bb49f154fb1f5d9698a146daed1204c5a442a190
AD\012020048D <reo-kano@sws.com>
2024-06-25 10:05:51 +0900

    【新規】CsvToXlsx.cs 追加

---
+++ CsvToXlsx.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//===================================================
+// 更新履歴
+//===================================================
+// 2024/06/20 : 012020048D : 新規追加
+//===================================================
+
+
+internal static class CsvToXlsx
+{
+
+
+}
+
